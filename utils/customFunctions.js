80dac10d7f872c893853ce607df66124efb91c6a
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2022-09-06 12:12:34 -0400

    removing unnecessary files

--- utils/customFunctions.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-module.exports = {
-   writeFile: function(filename, data){
-    fs.writeFile(filename, data, (err) => {
-        err ? console.log(err): console.log('success');
-    });
-   } 
-}
